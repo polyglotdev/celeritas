1fccbd2b5907e6d12bb7c85ab5f0b78452675c37
Dom Hallan <dom@domhallan.com>
2024-05-31 18:58:43 -0400

    Updated Render struct with ServerName
    The Render struct in the render package has been updated to include a new field, ServerName. This additional field will allow for more specific configuration of the renderer.

--- render/render.go
+++ render/render.go
@@ -1,8 +1,10 @@
 package render
 
+// Render is a struct that contains the configuration for the renderer.
 type Render struct {
-	Renderer string
-	RootPath string
-	Secure   bool
-	Port     string
+	Renderer   string
+	RootPath   string
+	Secure     bool
+	Port       string
+	ServerName string
 }
