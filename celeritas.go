ba8924e11139c65080e805f4380c904be7bac045
Dom Hallan <dom@domhallan.com>
2024-05-31 21:47:11 -0400

    Refactor createRender function in Celeritas
    The createRender function in the Celeritas class has been refactored. The function no longer takes a parameter and instead uses the instance's properties directly. Also, it now assigns the created render to the Render property of the instance itself rather than returning it.

--- celeritas.go
+++ celeritas.go
@@ -84,7 +84,7 @@ func (c *Celeritas) New(rootPath string) error {
 		renderer: os.Getenv("RENDERER"),
 	}
 
-	c.Render = c.createRender(c)
+	c.createRender()
 
 	return nil
 }
@@ -158,12 +158,11 @@ func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
 	return infoLog, errLog
 }
 
-func (c *Celeritas) createRender(cel *Celeritas) *render.Render {
+func (c *Celeritas) createRender() {
 	myRenderer := render.Render{
-		Renderer: cel.config.renderer,
-		RootPath: cel.RootPath,
-		Port:     cel.config.port,
+		Renderer: c.config.renderer,
+		RootPath: c.RootPath,
+		Port:     c.config.port,
 	}
-
-	return &myRenderer
+	c.Render = &myRenderer
 }
